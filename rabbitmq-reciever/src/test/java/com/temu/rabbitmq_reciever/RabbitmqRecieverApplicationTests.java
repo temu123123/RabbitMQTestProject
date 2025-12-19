57927c7a2f282d885d14c88ea7e8aa92a299ddc0
timur <timur@macbookpro.shitass.com>
2025-12-19 10:55:29 +0300

    Disable integration tests requiring RabbitMQ connection

--- rabbitmq-reciever/src/test/java/com/temu/rabbitmq_reciever/RabbitmqRecieverApplicationTests.java
+++ rabbitmq-reciever/src/test/java/com/temu/rabbitmq_reciever/RabbitmqRecieverApplicationTests.java
@@ -2,8 +2,10 @@ package com.temu.rabbitmq_reciever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
+@Disabled
 class RabbitmqRecieverApplicationTests {
 
 	@Test
--- rabbitmq/src/test/java/com/temu/rabbitmq/RabbitmqApplicationTests.java
+++ rabbitmq/src/test/java/com/temu/rabbitmq/RabbitmqApplicationTests.java
@@ -2,8 +2,10 @@ package com.temu.rabbitmq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
+@Disabled
 class RabbitmqApplicationTests {
 
 	@Test
